8ca9ab36670b2507b8c750628450cb586c41631c
marcscob <marcscob@hotmail.com>
2023-07-15 20:47:53 -0300

    check answer

--- simonGame.js
+++ simonGame.js
@@ -44,3 +44,16 @@ function animatePress(currentColour) {
     $("#" + currentColour).removeClass("pressed");
   });
 }
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log('success');
+    if (userClickedPattern.length === gamePattern.length){
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log( 'wrong')
+  }
+}
