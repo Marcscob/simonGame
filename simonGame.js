60e759ceb8c628bd89eec6fe8cac6cb8eef3d4e3
marcscob <marcscob@hotmail.com>
2023-07-15 17:21:44 -0300

    sound and animation

--- simonGame.js
+++ simonGame.js
@@ -2,9 +2,11 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-$('.btn').click(function(){
-  var userChosenColour = $(this).attr('id');
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
 });
 
 function nextSequence() {
@@ -12,9 +14,21 @@ function nextSequence() {
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColours);
 
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
- 
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  });
 }
