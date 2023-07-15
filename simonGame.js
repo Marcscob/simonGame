cff34931c708feada2c2a25a6df25a4ba773bdc6
marcscob <marcscob@hotmail.com>
2023-07-15 17:06:01 -0300

    just saving

--- simonGame.js
+++ simonGame.js
@@ -9,11 +9,12 @@ $('.btn').click(function(){
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColours = buttonColours[randomNumber];
+  var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColours);
 
-  $("#" + randomChosenColours).fadeIn(100).fadeOut(100).fadeIn(100);
-  var audio = new Audio("sounds/" + randomChosenColours + ".mp3");
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
  
 }
