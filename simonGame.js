5c31a6e518f8fba49896f86eed5e166f7461f8fc
marcscob <marcscob@hotmail.com>
2023-07-15 16:28:32 -0300

    animate and audio

---
+++ simonGame.js
@@ -0,0 +1,12 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColours = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColours);
+
+  $("#" + randomChosenColours).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio = new Audio("sounds/" + randomChosenColours + ".mp3");
+  audio.play();
+}
